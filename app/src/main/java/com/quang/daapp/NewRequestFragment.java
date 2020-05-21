cbdc9ada20a3e4bc3286d76fe47dac507ff7917f
Quang <quangnguyenvietminh@gmail.com>
2020-05-21 13:14:35 +0700

    Get Image From Gallery + Runtime Permission

--- app/src/main/java/com/quang/daapp/NewRequestFragment.java
+++ app/src/main/java/com/quang/daapp/NewRequestFragment.java
@@ -1,21 +1,36 @@
 package com.quang.daapp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -26,9 +41,11 @@ import java.util.Calendar;
  */
 public class NewRequestFragment extends Fragment {
 
+    private static final int GALLERY_REQUEST_CODE = 0;
     DatePickerDialog picker;
     TextView txtEndDate;
     Date choosenDate;
+    ImageView imageView;
 
     public NewRequestFragment() {
         // Required empty public constructor
@@ -38,7 +55,9 @@ public class NewRequestFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkPermissions();
+        }
         return inflater.inflate(R.layout.fragment_new_request, container, false);
     }
 
@@ -50,28 +69,85 @@ public class NewRequestFragment extends Fragment {
         final ImageButton btnChooseDate = view.findViewById(R.id.btnChooseDate);
         txtEndDate = view.findViewById(R.id.txtEnđate);
         changeDateDisplay(new Date(Calendar.getInstance().getTimeInMillis()));
+        imageView = view.findViewById(R.id.image_view);
         btnChooseDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final Calendar cldr = Calendar.getInstance();
-                cldr.setTime(choosenDate);
-                final int day = cldr.get(Calendar.DAY_OF_MONTH);
-                int month = cldr.get(Calendar.MONTH);
-                int year = cldr.get(Calendar.YEAR);
-
-                picker = new DatePickerDialog(getActivity(),
-                        new DatePickerDialog.OnDateSetListener() {
-                            @Override
-                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                                cldr.set(Calendar.YEAR,year);
-                                cldr.set(Calendar.MONTH,month);
-                                cldr.set(Calendar.DAY_OF_MONTH,dayOfMonth);
-                                changeDateDisplay(new Date(cldr.getTimeInMillis()));
-                            }
-                        }, year, month, day);
-                picker.show();
+                openDatePicker();
             }
         });
+
+        final Button btnChooseImg = view.findViewById(R.id.btnChooseImg);
+        btnChooseImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickFromGallery();
+            }
+        });
+    }
+
+    private void openDatePicker() {
+        final Calendar cldr = Calendar.getInstance();
+        cldr.setTime(choosenDate);
+        final int day = cldr.get(Calendar.DAY_OF_MONTH);
+        int month = cldr.get(Calendar.MONTH);
+        int year = cldr.get(Calendar.YEAR);
+
+        picker = new DatePickerDialog(getActivity(),
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        cldr.set(Calendar.YEAR,year);
+                        cldr.set(Calendar.MONTH,month);
+                        cldr.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+                        changeDateDisplay(new Date(cldr.getTimeInMillis()));
+                    }
+                }, year, month, day);
+        picker.show();
+    }
+
+    private void checkPermissions(){
+
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED||
+                ContextCompat.checkSelfPermission(getContext(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    },
+                    1052);
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1052: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted.
+
+                } else {
+
+
+                    // Permission denied - Show a message to inform the user that this app only works
+                    // with these permissions granted
+
+                }
+                return;
+            }
+
+        }
     }
 
     private void changeDateDisplay(Date date) {
@@ -79,5 +155,39 @@ public class NewRequestFragment extends Fragment {
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         format.format(date);
         txtEndDate.setText(date.toString());
+    }
+
+    private void pickFromGallery(){
+        //Create an Intent with action as ACTION_PICK
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        // Sets the type as image/*. This ensures only components of type image are selected
+        intent.setType("image/*");
+        //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        // Launching the Intent
+        startActivityForResult(intent,GALLERY_REQUEST_CODE);
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // Result code is RESULT_OK only if the user selects an Image
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case GALLERY_REQUEST_CODE:
+                    //data.getData returns the content URI for the selected Image
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                    Cursor cursor = getActivity().getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String picturePath = cursor.getString(columnIndex);
+                    cursor.close();
+                    imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                    break;
+            }
     }
 }
